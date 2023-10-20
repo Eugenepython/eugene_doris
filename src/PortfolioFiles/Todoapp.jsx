0201e0eb83dae723f8a0e8047a386ed951ece8b3
eugenepython <eugene_doris@hotmail.com>
2023-10-20 10:15:17 +0100

    changing the todo app

--- src/PortfolioFiles/Todoapp.jsx
+++ src/PortfolioFiles/Todoapp.jsx
@@ -8,14 +8,14 @@ function Todoapp() {
         <div>
         
         <h1>Task app</h1>
-        <p className = 'paragraph-container' >This is a task app linked to a postgresql database which stories the data and the authorisaion of emails. </p>
-        <p>It is built with node.js, postgreSQL, react and deployed on Kinsta. It is full stack with server and client side.</p>
+        <p className = 'paragraph-container' >This is a task app linked to a postgresql database which stores the data and the authorisaion of emails. </p>
+        <p>It is built with node.js, postgreSQL, react and deployed on Railway. It is full stack with server and client side.</p>
         <img className = 'portFolioImage' src={thetodoapp} alt="todo" />
         
         <p><a className = 'white' href="https://github.com/Eugenepython/todoapp" target="_blank"> Github page and source code</a></p>
        
         
-        <p><a className = 'white' href="https://taskappp-qbjry.kinsta.app/" target="_blank">Website hosted on Kinsta</a></p>
+        <p><a className = 'white' href="https://client-frontend-production.up.railway.app/" target="_blank">Website hosted on Railway</a></p>
 
         </div>
       </>
