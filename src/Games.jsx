64116510cbd31d32a3cd4d35ada29ab9d3035125
eugenepython <eugene_doris@hotmail.com>
2023-12-07 18:20:47 +0000

    updating santa

--- src/Games.jsx
+++ src/Games.jsx
@@ -7,17 +7,20 @@ import CrypticBond from './PortfolioFiles/CrypticBond.jsx';
 import TypingGame from './PortfolioFiles/TypingGame.jsx';
 import BondImageGame from './PortfolioFiles/BondImageGame.jsx';
 import CountDown from './PortfolioFiles/CountDown.jsx';
+import SecretSanta from './PortfolioFiles/SecretSanta.jsx';
 
 function Games() {
   return (
     <div>
-      <div className = 'folio'>  <CountDown />   </div> 
-      <div className = 'folio'>  <BondTenzies />   </div> 
-      <div className = 'folio'>     <Quizzical />    </div>
-      <div className = 'folio'>  <TintinGame />   </div>
-      <div className = 'folio'>  <CrypticBond />   </div> 
-      <div className = 'folio'>  <TypingGame />   </div> 
-      <div className = 'folio'>  <BondImageGame />   </div>
+
+      <div className='folio'>  <SecretSanta />   </div>
+      <div className='folio'>  <BondTenzies />   </div>
+      <div className='folio'>  <CountDown />   </div>
+      <div className='folio'>     <Quizzical />    </div>
+      <div className='folio'>  <TintinGame />   </div>
+      <div className='folio'>  <CrypticBond />   </div>
+      <div className='folio'>  <TypingGame />   </div>
+      <div className='folio'>  <BondImageGame />   </div>
     </div>
   );
 }
